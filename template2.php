95bd15e71fc076ba3612193ca1d3938dcd45ee52
aboulatif <latifbill86@gmail.com>
2024-11-11 08:40:08 +0000

    Update template2.php
    template

--- template2.php
+++ template2.php
@@ -1,1 +1,71 @@
 
+
+
+																								<table style="display: inline-block; width: 170px; border: 1px solid #ccc; line-height: 110%; font-family: arial; font-size: 16px; margin: 1px;"> 
+<tbody>
+<style>
+.qrcode{
+	height:90px;
+	width:90px;
+}
+</style>
+<tr>
+<td style="text-align: center; color: red; font-size: 13px; border-bottom: 1px #ccc solid;">
+<span id="num"><?= " [$num]"; ?></span>
+<b><?= $hotspotname; ?></b>
+</td> 
+</tr> 
+<tr> 
+<td> 
+<table style=" text-align: center; width: 160px; background-color: #fff; line-height: 110%; font-size: 13px;"> 
+<tbody>
+<tr> 
+<td>
+<?= $qrcode ?>
+</td> 
+</tr>
+<tr style="background-color: #eee;"> 
+<td style="background-color: #fff; width: 90%">
+<b>CODE TEMPS</b>
+</td> 
+</tr>
+<tr> 
+<td style="font-size: 20px;">
+<b><?= $username; ?></b>
+</td> 
+</tr> 
+</tbody> 
+</table> 
+</td> 
+</tr> 
+<tr> 
+<td> 
+<table style=" text-align: center; width: 160px; background-color: #fff; line-height: 110%; font-size: 13px;"> 
+<tbody> 
+<tr style="background-color: #eee;"> 
+<td style="width: 90%;"><b><?= $profile; ?></b></td> 
+</tr> 
+</tbody> 
+</table> 
+</td> 
+</tr> 
+<tr> 
+<td> 
+<table style=" text-align: center; width: 160px; background-color: #fff; line-height: 110%; font-size: 15px;"> 
+<tbody> 
+<tr style="background-color: #eee;"> 
+<td style="width: 90%;">
+<b><?= $price; ?> F CFA</b>
+</td> 
+</tr> 
+</tbody> 
+</table> 
+</td> 
+</tr>
+<tr> 
+<td style="text-align: center; font-size:13px; border-top: 1px #ccc solid;">
+05 44 54 42 43
+</td> 
+</tr> 
+</tbody> 
+</table>	
